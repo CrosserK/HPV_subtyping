9008122bf420e2ec6165ed58adea4ec5844da8fe
KasperH2 <45485501+KasperH2@users.noreply.github.com>
2022-11-02 14:42:09 +0100

    Delete Scripts directory

--- Scripts/Bash/MakeEmptyTxtFile.sh
+++
@@ -1,9 +0,0 @@
-
-# Laver tomme tekstfiler med samme navn som FASTQ filer i mappen FASTQ
-
-MainF=../..
-
-for f in $MainF/FASTQ/*.fastq; do
-	Name=$(basename $f) 
-	touch $MainF/References/InputRefs/${Name%fastq}txt
-done
--- Scripts/Python/Find_seq_position.py
+++
@@ -1,5 +0,0 @@
-
-with open("chr17_duplications_12_100.fa", 'r') as f:
-    content = f.read().replace("\n", "")
-    print(content.index('gtggaatgaataaacaaatgacagcacattcgtgcaatggaatatgctgctgaggtgaaaatgaatgggtgcacacagcatgcattaacatgcataaatc')-6)
-
